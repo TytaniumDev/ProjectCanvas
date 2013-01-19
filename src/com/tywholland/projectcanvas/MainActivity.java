802b8f306d12e634cd117c2ac4bb2ecd844c761e
Tyler Holland <tywholland@gmail.com>
2013-01-19 15:14:13 -0800

    Will show some images

--- src/com/tywholland/projectcanvas/MainActivity.java
+++ src/com/tywholland/projectcanvas/MainActivity.java
@@ -1,15 +1,66 @@
 package com.tywholland.projectcanvas;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	private TouchImageView mImageView;
+	private Button mButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mImageView = (TouchImageView) findViewById(R.id.imageView1);
+		mButton = (Button) findViewById(R.id.button1);
+		mButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("image/*");
+				startActivityForResult(intent, 0);
+			}
+		});
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent imageReturnedIntent) {
+		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+		switch (requestCode) {
+		case 0:
+			if (resultCode == RESULT_OK) {
+				Uri selectedImage = imageReturnedIntent.getData();
+				String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+				Cursor cursor = getContentResolver().query(selectedImage,
+						filePathColumn, null, null, null);
+				cursor.moveToFirst();
+
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				String filePath = cursor.getString(columnIndex); // file path of
+																	// selected
+																	// image
+				cursor.close();
+				// Convert file path into bitmap image using below line.
+				Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+
+				// put bitmapimage in your imageview
+				mImageView.setImageBitmap(yourSelectedImage);
+			}
+		}
 	}
 
 	@Override
